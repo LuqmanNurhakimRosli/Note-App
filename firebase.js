2fd251947fd48311faa53e36af69616704e8792c
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-31 10:29:52 +0800

    done

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKbtvAidsz4LRa147gjUxbSZSAUWaCXGc",
+  authDomain: "react-note-app-5d72b.firebaseapp.com",
+  projectId: "react-note-app-5d72b",
+  storageBucket: "react-note-app-5d72b.firebasestorage.app",
+  messagingSenderId: "77925555183",
+  appId: "1:77925555183:web:bbdc7ed43e0f6b2653b41a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- index.jsx
+++ index.jsx
@@ -1,7 +1,11 @@
 import React from "react"
-import { createRoot } from "react-dom/client" // Update this line
+import ReactDOM from "react-dom/client"
 import App from "./App"
 import './style.css'
 
-const root = createRoot(document.getElementById("root"))
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
